ecb6056d37abc8ee7d1f6291519f4b3730f7e8b3
Paul <cokecoder@gmail.com>
2013-06-08 22:34:42 +0800

    networking/ip-range

--- networking/ip-range/merge-ip-range.c
+++ networking/ip-range/merge-ip-range.c
@@ -4,17 +4,32 @@
 #include <string.h>
 #include <arpa/inet.h>
 
-int main()
+int main(int argc, char *argv[])
 {
     uint32_t a, b, lasta = 0, lastb = 0;
+    uint32_t tmp;
     char str1[128], str2[128];
     char laststr1[128], laststr2[128];
+    int ip_is_number = 0;
+    uint32_t total = 0;
+
+    if (argc > 1 && strcmp(argv[1], "--number") == 0) {
+        ip_is_number = 1;
+    }
 
     while (scanf("%s %s", str1, str2) == 2) {
-        inet_pton(AF_INET, str1, &a);
-        inet_pton(AF_INET, str2, &b);   // XXX check?
-        a = ntohl(a);
-        b = ntohl(b);
+        if (ip_is_number) {
+            a = strtoul(str1, NULL, 0);
+            b = strtoul(str2, NULL, 0);
+            tmp = htonl(a); inet_ntop(AF_INET, &tmp, str1, sizeof(str1));
+            tmp = htonl(b); inet_ntop(AF_INET, &tmp, str2, sizeof(str2));
+        }
+        else {
+            inet_pton(AF_INET, str1, &a);
+            inet_pton(AF_INET, str2, &b);   // XXX check?
+            a = ntohl(a);
+            b = ntohl(b);
+        }
 
         if (a == lastb || a == lastb + 1) {
             lastb = b;
@@ -23,6 +38,7 @@ int main()
         else {
             if (lasta) {
                 printf("%s %s\n", laststr1, laststr2);
+                total += lastb - lasta + 1;
             }
             lasta = a;
             lastb = b;
@@ -33,5 +49,9 @@ int main()
 
     if (lasta) {
         printf("%s %s\n", laststr1, laststr2);
+        total += lastb - lasta + 1;
     }
+
+    fprintf(stderr, "Total: %u IPs\n", total);
+    return 0;
 }
--- networking/ip-range/print-ip.c
+++ networking/ip-range/print-ip.c
@@ -2,26 +2,59 @@
 #include <stdio.h>
 #include <arpa/inet.h>
 
+/* IP should be network-byteorder */
+void print_ip(uint32_t ip)
+{
+    char buf[1024];
+    uint32_t num;
+    int i;
+
+    num = ntohl(ip);
+    printf("%u\n", num);
+    printf("%08X\n", num);
+
+    for (i = 31; i >= 0; i--) {
+        if (num & (1 << i))
+            putchar('1');
+        else
+            putchar('0');
+        if (i > 0 && i % 8 == 0)
+            putchar(' ');
+    }
+    putchar('\n');
+
+    if (inet_ntop(AF_INET, &ip, buf, sizeof(buf)) != NULL) {
+        printf("%s\n", buf);
+    }
+    else {
+        printf("Invalid IP.\n");
+    }
+}
+
 int main(int argc, char *argv[])
 {
     uint32_t ip;
-    int i;
+    const char * arg = argv[1];
 
-    if (inet_pton(AF_INET, argv[1], &ip) == 1) {
+    if (inet_pton(AF_INET, arg, &ip) == 1) { /* dotted decimal */
         printf("Parsed IP OK.\n");
-        ip = ntohl(ip);
-        printf("%u\n", ip);
-        printf("%08X\n", ip);
-
-        for (i = 31; i >= 0; i--) {
-            if (ip & (1 << i))
-                putchar('1');
-            else
-                putchar('0');
-            if (i > 0 && i % 8 == 0)
-                putchar(' ');
-        }
-        putchar('\n');
+        print_ip(ip);
     }
+    else {
+        /* try number */
+        ip = strtoul(arg, NULL, 10);
+        if (ip > 0) {
+            print_ip(htonl(ip));
+            return 0;
+        }
+
+        /* try hexcimal */
+        ip = strtoul(arg, NULL, 16);
+        if (ip > 0) {
+            print_ip(htonl(ip));
+            return 0;
+        }
+    }
+
     return 0;
 }
