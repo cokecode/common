74ccf7066666958a55efffa305b523f948d454aa
Paul <cokecoder@gmail.com>
2013-06-08 22:37:05 +0800

    getopt

--- misc/getopt/getopt_example.c
+++ misc/getopt/getopt_example.c
@@ -1,17 +1,40 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <getopt.h>
+#include <string.h>
+
+struct {
+    int daemon;
+    char cfgfile[1024];
+} g_opts;
+
+static void print_usage()
+{
+    printf("help message ...\n");
+}
+
+static void print_version()
+{
+    printf("version ...\n");
+}
+
 static void parse_cmdline(int argc, char * argv[])
 {
 	struct option opts[] =
 	{
-		{ "--help", 0, NULL, 'h', },
-		{ "--version", 0, NULL, 'v', },
-		{ "--foreground", 0, NULL, 'x', },
-		{ "--terminate", 0, NULL, 't', },
-		{ "--config", 1, NULL, 'f', },
+		{ "help", 0, NULL, 'h' },
+		{ "version", 0, NULL, 'v' },
+		{ "foreground", 0, NULL, 'x' },
+		{ "terminate", 0, NULL, 't' },
+		{ "config", 1, NULL, 'f' },
+        { "long-only1", 1, NULL, 0 },
+        { "long-only2", 0, NULL, 0 },
 		{ NULL, 0, NULL, 0, },
 	};
 	char c;
+    int longidx = -1;
 
-	while ((c = getopt_long(argc, argv, "hvtxf:", opts, NULL)) != -1)
+	while ((c = getopt_long(argc, argv, "hvtxf:", opts, &longidx)) != -1)
 	{
 		switch (c)
 		{
@@ -22,7 +45,7 @@ static void parse_cmdline(int argc, char * argv[])
 				print_version();
 				exit(1);
 			case 'x':
-				g_opts.daemon = false;
+				g_opts.daemon = 0;
 				break;
 			case 't':
 				printf("ignore -t...\n");
@@ -32,8 +55,21 @@ static void parse_cmdline(int argc, char * argv[])
 				break;
 			case '?':
 			default:
-				// getopt_long() has printed some error message.
-				exit(1);
+                printf("longidx = %d\n", longidx);
+                if (longidx != -1) {
+                    printf("got option %s\n", opts[longidx].name);
+                }
+                else {
+                    // getopt_long() has printed some error message.
+                    exit(1);
+                }
 		}
 	}
 }
+
+int main(int argc, char *argv[])
+{
+    parse_cmdline(argc, argv);
+    return 0;
+}
+
