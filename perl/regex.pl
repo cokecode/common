ba7d33ef0c3e6975fa4ae374b1359433014e6346
Paul <cokecoder@gmail.com>
2013-09-03 09:12:16 +0800

    .

---
+++ perl/regex.pl
@@ -0,0 +1,26 @@
+#! /usr/bin/perl -w
+
+use strict;
+
+my $string = "Hello, world!";
+
+my $result = $string =~ s/wor/WOR/;
+# $result should be: 1 (success)
+
+print $result? "Success" : "Failure", "\n";
+
+$result = $string =~ s/wor/WOR/;
+# $result should be: undef (failure)
+
+print $result? "Success" : "Failure", "\n";
+
+=comment
+
+$result = $string =~ s/WOR/WAR/r;	# Note: s/../../r
+# $string is unchanged.
+# $result should be: Hello, WARld!
+
+print "$result\n";
+
+=cut
+
