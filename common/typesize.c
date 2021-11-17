72a26fb2e3f317893d31d8108d808fa815c0745d
seatle <seatle@my-macbook.local>
2021-11-17 22:59:49 +0800

    show more type size.

--- common/typesize.c
+++ common/typesize.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <time.h>
 
-#define PSIZE(x)	printf(#x" = %d\n", x)
+#define PSIZE(x)	printf(#x" = %lu\n", x)
 
 int main()
 {
+	PSIZE(sizeof(char));
 	PSIZE(sizeof(short));
 	PSIZE(sizeof(int));
 	PSIZE(sizeof(long));
@@ -12,7 +14,11 @@ int main()
 	PSIZE(sizeof(int16_t));
 	PSIZE(sizeof(int32_t));
 	PSIZE(sizeof(int64_t));
+	PSIZE(sizeof(size_t));
+	PSIZE(sizeof(ssize_t));
 	PSIZE(sizeof(float));
 	PSIZE(sizeof(double));
 	PSIZE(sizeof(long double));
+	PSIZE(sizeof(time_t));
+	return 0;
 }
